9edb1259dd5a00837929cc024b4aa8c2de331e70
Himanshu Maurya <himanshu.mmmut@gmail.com>
2020-10-05 00:35:56 +0530

    Sample Commit

---
+++ Number Theory/checkOddEven.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n&1)
+        cout<<"ODD Number\n";
+    else
+        cout<<"Even Number\n";
+    return 0;
+}
