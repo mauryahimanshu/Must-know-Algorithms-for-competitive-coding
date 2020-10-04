2fb97139c64f495c4cac883237dda8cf2248a043
GitanshKapoor <72307552+GitanshKapoor@users.noreply.github.com>
2020-10-05 01:55:05 +0530

    Update fibonacciSeries.cpp

--- Number Theory/fibonacciSeries.cpp
+++ Number Theory/fibonacciSeries.cpp
@@ -1,29 +1,16 @@
-#include <iostream>
 #include <bits/c++.h>
 using namespace std;
 
+int fib(int n)
+{
+    if (n <= 1) 
+        return n; 
+    return fib(n-1) + fib(n-2); 
+}
+
 int main()
 {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
-    cin >> n;
-    cout << "Fibonacci Series: ";
-    for (int i = 1; i <= n; ++i)
-    {
-        if(i == 1)
-        {
-            cout << " " << t1;
-            continue;
-        }
-        if(i == 2)
-        {
-            cout << t2 << " ";
-            continue;
-        }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-        
-        cout << nextTerm << " ";
-    }
-    return 0;
+    int n;
+    cin>>n;
+    cout<<fib(n);
 }
