8b6b0eaddfc0a342d48cbb760d1c453a2b230d6b
SaimantikaBasu <67591643+SaimantikaBasu@users.noreply.github.com>
2020-10-07 22:29:44 +0530

    Update bipartite.cc

--- Graph/bipartite.cc
+++ Graph/bipartite.cc
@@ -8,65 +8,50 @@ using namespace std;
 
 // This function returns true if graph 
 // G[V][V] is Bipartite, else false 
-bool isBipartite(int G[][V], int src) 
-{ 
-	// Create a color array to store colors 
-	// assigned to all veritces. Vertex 
-	// number is used as index in this array. 
-	// The value '-1' of colorArr[i] 
-	// is used to indicate that no color 
-	// is assigned to vertex 'i'. The value 1 
-	// is used to indicate first color 
-	// is assigned and value 0 indicates 
-	// second color is assigned. 
-	int colorArr[V]; 
-	for (int i = 0; i < V; ++i) 
-		colorArr[i] = -1; 
-
-	// Assign first color to source 
-	colorArr[src] = 1; 
-
-	// Create a queue (FIFO) of vertex 
-	// numbers and enqueue source vertex 
-	// for BFS traversal 
-	queue <int> q; 
-	q.push(src); 
-
-	// Run while there are vertices 
-	// in queue (Similar to BFS) 
-	while (!q.empty()) 
-	{ 
-		// Dequeue a vertex from queue ( Refer http://goo.gl/35oz8 ) 
-		int u = q.front(); 
-		q.pop(); 
-
-		// Return false if there is a self-loop 
-		if (G[u][u] == 1) 
-		return false; 
-
-		// Find all non-colored adjacent vertices 
-		for (int v = 0; v < V; ++v) 
-		{ 
-			// An edge from u to v exists and 
-			// destination v is not colored 
-			if (G[u][v] && colorArr[v] == -1) 
-			{ 
-				// Assign alternate color to this adjacent v of u 
-				colorArr[v] = 1 - colorArr[u]; 
-				q.push(v); 
-			} 
-
-			// An edge from u to v exists and destination 
-			// v is colored with same color as u 
-			else if (G[u][v] && colorArr[v] == colorArr[u]) 
-				return false; 
-		} 
-	} 
-
-	// If we reach here, then all adjacent 
-	// vertices can be colored with alternate color 
-	return true; 
-} 
+bool bfs(int src,vector<int> &vis,vector<vector<int>>& graph)
+    {
+        queue<pair<int,int>> que;
+        que.push({src,0});
+        
+        while(que.size()!=0)
+        {
+            int size=que.size();
+            while(size-- > 0)
+            {
+                pair<int,int> rvtx=que.front();
+                que.pop();
+                if(vis[rvtx.first]!=-1)
+                {
+                    if(vis[rvtx.first]!=rvtx.second)
+                        return false;
+                }
+                vis[rvtx.first]=rvtx.second;
+                int color=(rvtx.second+1)%2; //assigns colour in the form of digits
+                for(int e : graph[rvtx.first])
+                {
+                    if(vis[e]==-1)
+                    {
+                        que.push({e,color});
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> vis(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            if(graph[i].size()==0 || vis[i]!=-1)
+                continue;
+            if(!bfs(i,vis,graph))
+                return false;
+        }
+        
+        return true;
+   }
 
 // Driver program to test above function 
 int main() 
@@ -77,6 +62,6 @@ int main()
 		{1, 0, 1, 0} 
 	}; 
 
-	isBipartite(G, 0) ? cout << "Yes" : cout << "No"; 
+	isBipartite(G) ? cout << "Yes" : cout << "No"; 
 	return 0; 
 } 
